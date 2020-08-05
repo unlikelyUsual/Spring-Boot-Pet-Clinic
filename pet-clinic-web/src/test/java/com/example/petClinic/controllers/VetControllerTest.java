5fcc36beca8c772eb6c5ddd5b295561e9a63a4b9
Prabhat <prabhat.tanwar@twinline.in>
2020-08-05 22:27:09 +0530

    Added controller MockMVC Test.Closes #16

---
+++ pet-clinic-web/src/test/java/com/example/petClinic/controllers/VetControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.petClinic.controllers;
+
+import com.example.petClinic.model.Vet;
+import com.example.petClinic.services.VetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    VetService vetService;
+
+    @InjectMocks
+    VetController vetController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+       mockMvc = MockMvcBuilders.standaloneSetup(vetController).build();
+    }
+
+    @Test
+    void getIndexPage() throws Exception {
+        Vet vet = new Vet();
+        vet.setId(1L);
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        Set<Vet> vets = new HashSet<>();
+        vets.add(vet);
+        vets.add(vet2);
+        when(vetService.findAll()).thenReturn(vets);
+
+        mockMvc.perform(get("/vet/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets",hasSize(2)));
+    }
+}
