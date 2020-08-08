5f12c67be0605e252978abc8ef5c0c00e29ed74c
Prabhat <prabhat.tanwar@twinline.in>
2020-08-08 23:22:22 +0530

    Fixed Menu . Closes #8

--- pet-clinic-web/src/main/java/com/example/petClinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petClinic/controllers/OwnerController.java
@@ -4,6 +4,7 @@ import com.example.petClinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +21,12 @@ public class OwnerController {
     String getIndexPage(Model model){
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/view/{id}")
+    String viewOwnerDetail(@PathVariable("id") Long id,  Model model) {
+        model.addAttribute("owner",ownerService.findById(id));
+        return "owners/viewOwner";
     }
 
 }
--- pet-clinic-web/src/main/java/com/example/petClinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petClinic/controllers/VetController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "vet")
+@RequestMapping(value = "vets")
 public class VetController {
 
     private final VetService vetService;
