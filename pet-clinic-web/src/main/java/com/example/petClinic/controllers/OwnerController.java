58a1876331d547bce1b40945ca0133701fcafc31
Prabhat Kumar <heywhat.sup308@gmail.com>
2020-08-29 22:48:43 +0530

    owner update Fix

--- pet-clinic-web/src/main/java/com/example/petClinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petClinic/controllers/OwnerController.java
@@ -5,11 +5,9 @@ import com.example.petClinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -72,7 +70,7 @@ public class OwnerController {
     }
 
     @PostMapping("/addOrUpdate")
-    String saveOrUpdateOwner(Owner owner , BindingResult result , Model model){
+    String saveOrUpdateOwner(@Valid @ModelAttribute("owner") Owner owner , BindingResult result , Model model){
         if(result.hasErrors()) return "owners/ownerForm";
         Owner saveOwner = ownerService.save(owner);
         return  "redirect:/owner/view/" + saveOwner.getId();
